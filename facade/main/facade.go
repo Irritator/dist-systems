2948fb45f3d8da670861efc8147b97ed51acb7c4
vitalii.aheienko <vitalii.ageenko@rm.imshealth.com>
2021-04-10 14:07:56 +0300

    process errors

--- facade/main/facade.go
+++ facade/main/facade.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"github.com/google/uuid"
 	"io/ioutil"
 	"net/http"
@@ -17,11 +16,9 @@ func main() {
 type FacadeListener struct{}
 
 func (m *FacadeListener) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	fmt.Println("\n\n\n==========================")
-	fmt.Println("FacadeListener")
+	var responseBody string
 	if request.Method == "GET" {
-		responseBody := service.GetData(service.LoggingServiceAddr) + "\n" + service.GetData(service.MessagesServiceAddr)
-		_, _ = writer.Write([]byte(responseBody))
+		responseBody = service.GetData(service.LoggingServiceAddr) + "\n" + service.GetData(service.MessagesServiceAddr)
 	} else if request.Method == "POST" {
 		body, err := ioutil.ReadAll(request.Body)
 		if err != nil {
@@ -34,9 +31,15 @@ func (m *FacadeListener) ServeHTTP(writer http.ResponseWriter, request *http.Req
 		}
 		info := service.RequestInfo{uuid.New().String(), params.Msg}
 		logRequestMessage, _ := json.Marshal(info)
-		_, _ = http.Post(
+		_, err = http.Post(
 			service.Localhost+service.LoggingServiceAddr,
 			"application/json",
 			bytes.NewReader(logRequestMessage))
+		if err != nil {
+			responseBody = "There is an error processing your data"
+		} else {
+			responseBody = "Message sent successfully!"
+		}
 	}
+	_, _ = writer.Write([]byte(responseBody))
 }
--- messagingService/main/messagingService.go
+++ messagingService/main/messagingService.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"service"
 )
@@ -13,8 +12,6 @@ func main() {
 type MessagingListener struct{}
 
 func (m *MessagingListener) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	fmt.Println("==========================")
-	fmt.Println("MessagingListener")
 	if request.Method == "GET" {
 		_, _ = writer.Write([]byte("Messaging Service currently unavailable"))
 	}
